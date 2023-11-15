759f81af1097432175daa5f981bf0be9883029af
jceplis3 <jceplis3@DXRDA201692.ri.riga.lv>
2023-11-15 09:06:28 +0200

    javascript

---
+++ js/original.js
@@ -0,0 +1,11 @@
+let wordBox = document.getElementsByClassName("org-word");
+let startBtn = document.getElementById("startBtn");
+function startGame(){
+    startBtn.classList.toggle("visible");
+    wordBox.classList.toggle("visible");
+    startTimer();
+}
+
+function startTimer(){
+    
+}

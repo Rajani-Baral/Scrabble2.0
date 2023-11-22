1ead5465ab30c3ea7a044a3051815bad0bfdf490
jceplis3 <jceplis3@DXRDA201692.ri.riga.lv>
2023-11-22 09:51:10 +0200

    22.11.2023

--- js/original.js
+++ js/original.js
@@ -4,9 +4,29 @@ let timer = document.getElementById("timer");
 function startGame(){
     startBtn.classList.toggle("visible");
     wordBox.classList.toggle("visible");
-    setTimeout(setTimeDiv(), 1000); //every second run setTimeDiv()
+    setInterval(setTimeDiv(), 1000); //every second run setTimeDiv()
 }
 
 function setTimeDiv(){
     timer.innerHTML="newtext";
 }
+
+function getFullscreenElement(){
+    return document.fullscreenElement
+        || document.webkitFullscreenElement
+        || document.mozFullscreenElement
+        || document.msFullscreenElement;
+}
+
+function toggleFullscreen(){
+    console.log("Smth is happeneing");
+    if (getFullscreenElement()) {
+            document.exitFullscreen();
+    }else {
+        document.getElementById("playContainer").requestFullscreen().catch(console.log) ;
+    }
+}
+
+document.getElementById("fullscreenBtn").addEventListener("click", () => {
+    toggleFullscreen();
+});
